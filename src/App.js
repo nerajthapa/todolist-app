ef2d49afd92f61cccc8eb20bd07e0aac08831372
Neeraj Kumar <neeraj.kumar@methodicatech.com>
2023-12-20 15:24:14 +0530

    Todo list app using react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,150 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid'; // Import uuid
+
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+  const [selectedTodo, setSelectedTodo] = useState(null);
+
+  useEffect(() => {
+    // Fetch todo items from the API
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=3')
+      .then(response => response.json())
+      .then(data => setTodos(data))
+      .catch(error => console.error('Error fetching todos:', error));
+  }, []);
+
+  const handleAddTodo = () => {
+    // Generate a unique ID using uuid
+    const newTodoId = uuidv4();
+
+    // Update the state with the new todo
+    setTodos(prevTodos => [
+      ...prevTodos,
+      {
+        userId: 1,
+        id: newTodoId,
+        title: newTodo,
+        completed: false,
+        manuallyAdded: true,
+      },
+    ]);
+
+    // Clear the input field
+    setNewTodo('');
+  };
+
+  const handleUpdateTodo = () => {
+    if (selectedTodo) {
+      if (selectedTodo.manuallyAdded) {
+        // If the todo was manually added, update it directly in the state
+        const updatedTodos = todos.map(todo =>
+          todo.id === selectedTodo.id ? { ...todo, title: newTodo } : todo
+        );
+        setTodos(updatedTodos);
+        setNewTodo('');
+        setSelectedTodo(null);
+      } else {
+        // Dummy request to update a todo (PUT request)
+        fetch(`https://jsonplaceholder.typicode.com/todos/${selectedTodo.id}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            ...selectedTodo,
+            title: newTodo,
+          }),
+        })
+          .then(response => response.json())
+          .then(data => {
+            // Update the state with the updated todo
+            const updatedTodos = todos.map(todo =>
+              todo.id === data.id ? data : todo
+            );
+            setTodos(updatedTodos);
+            setNewTodo('');
+            setSelectedTodo(null);
+          })
+          .catch(error => console.error('Error updating todo:', error));
+      }
+    }
+  };
+
+  const handleDeleteTodo = () => {
+    if (selectedTodo) {
+      if (selectedTodo.manuallyAdded) {
+        // If the todo was manually added, remove it directly from the state
+        const updatedTodos = todos.filter(todo => todo.id !== selectedTodo.id);
+        setTodos(updatedTodos);
+        setNewTodo('');
+        setSelectedTodo(null);
+      } else {
+        // Dummy request to delete a todo (DELETE request)
+        fetch(`https://jsonplaceholder.typicode.com/todos/${selectedTodo.id}`, {
+          method: 'DELETE',
+        })
+          .then(() => {
+            // Update the state by removing the deleted todo
+            const updatedTodos = todos.filter(
+              todo => todo.id !== selectedTodo.id
+            );
+            setTodos(updatedTodos);
+            setNewTodo('');
+            setSelectedTodo(null);
+          })
+          .catch(error => console.error('Error deleting todo:', error));
+      }
+    }
+  };
+
+  const handleSelectTodo = todo => {
+    // Set the selected todo for editing
+    setSelectedTodo(todo);
+    setNewTodo(todo.title);
+    // Toggle the completed status
+    const updatedTodos = todos.map(t =>
+        t.id === todo.id ? { ...t, completed: !t.completed } : t
+    );
+    setTodos(updatedTodos);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo App</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Add or Update a todo"
+          value={newTodo}
+          onChange={e => setNewTodo(e.target.value)}
+        />
+        <button onClick={selectedTodo ? handleUpdateTodo : handleAddTodo}>
+          {selectedTodo ? 'Update Todo' : 'Add Todo'}
+        </button>
+        {selectedTodo && (
+          <button onClick={handleDeleteTodo}>Delete Todo</button>
+        )}
+      </div>
+      <ul>
+        {todos.map(todo => (
+          <li
+            key={todo.id}
+            className={todo.deleted ? 'deleted' : ''}
+            onClick={() => handleSelectTodo(todo)}
+          >
+            <input
+              type="checkbox"
+              readOnly
+              checked={todo.completed}
+              onClick={() => handleSelectTodo(todo)}
+            />
+            <span>{todo.title}</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/readme.md
@@ -0,0 +1,48 @@
+# React Todo App
+
+## Overview
+
+This is a simple React Todo App that allows users to add, update, and delete todo items. The app fetches todo data from a placeholder API and supports CRUD operations. The design is kept clean and user-friendly.
+
+## Folder Structure
+
+The project follows a basic folder structure:
+
+/todo-app
+|-- src
+|-- App.js
+|-- App.css
+|-- public
+|-- index.html
+|-- package.json
+|-- README.md
+
+
+- `/src`: Contains the main application files.
+  - `App.js`: Implements the core functionality of the Todo App.
+  - `App.css`: Provides styles for the Todo App.
+- `/public`: Contains the main HTML file.
+  - `index.html`: The HTML file where the React app is mounted.
+- `package.json`: Configuration file for Node.js projects.
+- `README.md`: Documentation file with information about the project.
+
+## How to Start the Project
+
+To run the React Todo App locally, follow these steps:
+
+1. **Clone the Repository:**
+
+   ```bash
+   git clone https://github.com/your-username/todo-app.git
+2. **Navigate to the Project Directory:**
+    ```bash
+    cd todo-app
+3. **Install Dependencies:**
+    ```bash
+    npm install
+4. **Run the App:**
+    ```bash
+    npm start
+
+This will start the development server, and the app will be accessible at http://localhost:3000 in your browser.
+
